80522bf52b2a4fba43b29a2d7f1d8791b2c0c8c0
TheKacho <abyssknightzero@gmail.com>
2023-03-18 15:05:47 -0400

    Add project files.

---
+++ SeleniumTestProject/NewTab.cs
@@ -0,0 +1,46 @@
+﻿using FluentAssertions;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTestProject
+{
+    public class NewTab
+    {
+        [Fact]
+        public void NewTabTest()
+        {
+            string url = "https://demoqa.com/books";
+            //to reduce failure rate, simplify the links to the source that you're targeting
+
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+            string id = "see-book-Git Pocket Guide";
+            //string xpath = "(//span[contains(@id, 'see-book-')])";
+            //for Xpath, it is generally best to search for ID first
+
+            //var bookLink = driver.FindElements(By.XPath(xpath));
+
+            var bookLink = driver.FindElement(By.Id(id));
+            bookLink.Click();
+
+            string url2 = "https://demoqa.com/books?book=9781449325862";
+
+            driver.Url.Should().Contain("books?book=");
+
+            string id2 = "userName-value";
+            var websiteLink = driver.FindElement(By.Id(id2));
+            websiteLink.Click();
+
+            driver.WindowHandles.Should().HaveCount(1);
+
+            driver.Quit();
+
+        }
+    }
+}
---
+++ SeleniumTestProject/PageObjectModels/HomePage.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumTestProject.PageObjectModels
+{
+    internal class HomePage
+    {
+        readonly IWebDriver _driver;
+        readonly WebDriverWait _wait;
+        public string Url = "https://www.google.com";
+        public string Title = "Google";
+
+        public HomePage(IWebDriver driver)
+        {
+            //this method calls the web driver, waits for 6 seconds to load to the page and maximizes the browser window
+            _driver = driver;
+            _wait = new WebDriverWait(driver, TimeSpan.FromSeconds(6));
+            _driver.Manage().Window.Maximize();
+            _driver.Navigate().GoToUrl(Url);
+            EnsurePageLoad();
+        }
+
+        private void EnsurePageLoad()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
