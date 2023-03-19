f90c21fb6f310367de048a09247c20c6bbfff48b
TheKacho <abyssknightzero@gmail.com>
2023-03-19 15:26:21 -0400

    added more page model objects

---
+++ SeleniumTestProject/PageObjectModels/ArtOfPants.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTestProject.PageObjectModels
+{
+    internal class ArtOfPants
+    {
+
+        public string firstUrl = "https://theartofpants.com/";
+
+        string twitterXpathLink = "//*[@id=\"shopify-section-footer\"]/footer/div[1]/div[2]/ul/li[1]/a";
+        string instagramXpathLink = "//*[@id=\"shopify-section-footer\"]/footer/div[1]/div[2]/ul/li[2]/a";
+        internal By twitterButton { get => By.XPath(twitterXpathLink); }
+        internal By instagramButton { get => By.XPath(instagramXpathLink);}
+
+        
+
+    }
+}
---
+++ SeleniumTestProject/PageObjectModels/InstaGPants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTestProject.PageObjectModels
+{
+    internal class InstaGPants
+    {
+        internal string instaUrl = "https://www.instagram.com/pantspants/";
+
+        internal string instaTitle = "pants (@pantspants) · Instagram photos and videos";
+
+        string InstaUserUrlXpathLink = "//*[@id=\"mount_0_0_U4\"]/div/div/div[1]/div/div/div/div[1]/section/main/div/div[1]/a";
+    }
+}
