6b8ab955a2a564fbea5a567ed678988bd11f7615
TheKacho <abyssknightzero@gmail.com>
2023-03-29 23:21:58 -0400

    added another few page classes

--- SeleniumTestProject/PageObjectModels/LumaPage.cs
+++ SeleniumTestProject/PageObjectModels/LumaPage.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,7 +22,47 @@ namespace SeleniumTestProject.PageObjectModels
 
         public void Login(string email, string password)
         {
-
+            driver.FindElement(emailField).SendKeys(email);
+            driver.FindElement(passwordField).SendKeys(password);
+            driver.FindElement(loginButton).Click();
         }
+
+    }
+    public class ShirtPage
+    {
+        private IWebDriver driver;
+        private By addToCartBtn = By.CssSelector("[data-id='2'] .product-item-actions .action.tocart");
+
+        public ShirtPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void AddShirtToCart()
+        {
+            driver.FindElement(addToCartBtn).Click();
+        }      
+        
+    }
+
+    public class ShopCartPage
+    {
+        private IWebDriver driver;
+        private By quantityField = By.CssSelector("#shopping-cart-table tbody tr:nth-child(1) td:nth-child(4) input");
+        private By updateQuantityBtn = By.CssSelector("#shopping-cart-table tbody tr:nth-child(1) td:nth-child(4) button");
+
+        public ShopCartPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void UpdateQuantity(int quantity)
+        {
+            driver.FindElement(quantityField).Clear();
+            driver.FindElement(quantityField).SendKeys(quantity.ToString());
+            driver.FindElement(updateQuantityBtn).Click();
+        }
+
+
     }
 }
