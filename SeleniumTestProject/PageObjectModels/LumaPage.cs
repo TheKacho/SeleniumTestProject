fe8c864a71a40d9cdc2be8bc44bc97ce51716afc
TheKacho <abyssknightzero@gmail.com>
2023-03-29 22:55:27 -0400

    adding a new POM for a new test

---
+++ SeleniumTestProject/PageObjectModels/LumaPage.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTestProject.PageObjectModels
+{
+    public class LumaPage
+    {
+        private IWebDriver driver;
+        private By emailField = By.Id("email");
+        private By passwordField = By.Id("pass");
+        private By loginButton = By.Id("send2");
+
+        public LumaPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void Login(string email, string password)
+        {
+
+        }
+    }
+}
